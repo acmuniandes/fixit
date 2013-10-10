dd6a208bd572af9d0fc51415e6036dfe6111466c
fernandohur <fernandohur@gmail.com>
2013-10-09 23:34:45 -0500

    added helper to application_helper render_notice which allows rendering of bootstrap style alerts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,15 @@ module ApplicationHelper
 	def render_navbar
 		render :partial=>'common/navbar'
 	end
+
+
+	# renders an alert message
+	# alert_type (String) can be one of alert-success, 
+	# alert-info, alert-warning, alert-danger
+	def render_notice(title, message, alert_type="alert-info")
+		alert_types = ["alert-success", "alert-info", "alert-warning", "alert-danger"]
+		raise ArgumentError, "alert_type must be one of #{alert_types}" unless alert_types.include? alert_type
+		render :partial=>"common/flash",:locals=>{:title=>title,:alert_type=>alert_type,:message=>message}		
+	end
+
 end
